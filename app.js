5214f19cdfc923bc9fa46707ba457fe026a6fd41
Oladeji Israel Oluwakayode <oladeji821@gmail.com>
2022-09-15 21:09:00 +0100

    I create a function with an event listener

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ let count = 0
 
 // Set value and button
 
-const value = document.querySelector(#'value');
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener(click, function(e){
+        
+    })
+})
 
